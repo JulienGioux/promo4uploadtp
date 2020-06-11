7496f3c443f37cdd08baee02aa81197996c070a8
unknown <pauline.desert@hotmail.fr>
2020-06-11 12:14:42 +0200

    Commit ++

--- index.php
+++ index.php
@@ -1,5 +1,39 @@
+<?php
+
+$tempPath = $_FILES['myImg']['tmp_name'];
+$actualSize = $_FILES['myImg']['size'];
+$mimeType = $_FILES['myImg']['type'];
+$infoExtension = pathinfo($_FILES['myImg']['name']);
+$actualExtension = $infoExtension['extension'];
+$newName = uniqid('img_');
+$path = './img';
+
+$extensionAccepted = ['image/jpg', 'image/png'];
+$sizeMax = '100000';
+
+echo $mimeType;
+
+$i = 0;
+do {
+    $i++;
+    $newName = uniqid('img_');
+} while (file_exists($path . '/' . $newName . '.' . $actualExtension) && $i < 10);
+
+if (in_array($mimeType, $extensionAccepted) && $i < 10) {
+        move_uploaded_file($tempPath, $path . '/' . $newName . '.' . $actualExtension);
+}
+
+
+
+
+
+var_dump($_FILES);
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,6 +43,7 @@
     <link rel="stylesheet" href="assets/style.css">
     <title>TP Upload</title>
 </head>
+
 <body>
     <div class="row">
         <div class="col s12 m10 offset-m1">
@@ -16,21 +51,21 @@
             <p>Mise en pratique PHP : Upload d'images.</p>
             <div class="card horizontal">
                 <div class="card-stacked">
-                <form action="#">
-                    <div class="file-field input-field">
-                        <p>Veuillez choisir une image :</p>
-                        <div class="btn">
-                            <span>File</span>
-                            <input type="file">
+                    <form action="index.php" method="post" enctype="multipart/form-data">
+                        <div class="file-field input-field">
+                            <p>Veuillez choisir une image :</p>
+                            <div class="btn">
+                                <span>File</span>
+                                <input type="file" id="myImg" name="myImg">
+                            </div>
+                            <div class="file-path-wrapper">
+                                <input class="file-path validate" type="text">
+                            </div>
                         </div>
-                        <div class="file-path-wrapper">
-                            <input class="file-path validate" type="text">
-                        </div>
-                    </div>
-                    <button class="btn waves-effect waves-light" type="submit" name="action">Submit
-                        <i class="material-icons right">send</i>
-                    </button>
-                </form>
+                        <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+                            <i class="material-icons right">send</i>
+                        </button>
+                    </form>
                 </div>
                 <div class="card-image">
                     <img src="img/capture01.PNG">
@@ -42,4 +77,5 @@
     <script src="uploadPreview/uploadPreview.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
+
 </html>
