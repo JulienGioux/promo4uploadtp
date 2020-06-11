897bd8d65a2ef3eed369c72cbf91e3775300b6aa
unknown <jules.gioux@gmail.com>
2020-06-11 11:15:19 +0200

    preview

--- index.php
+++ index.php
@@ -36,13 +36,15 @@
                 </form>
                 </div>
                 <div class="card-image">
-                    <img class="preview" src="img/capture01.PNG">
+                    <img class="preview" src="img/capture01.png">
                 </div>
             </div>
         </div>
     </div>
 
-    <script src="assets/uploadPreview.js"></script>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="assets/uploadPreview.js"></script>
 </body>
 </html>
