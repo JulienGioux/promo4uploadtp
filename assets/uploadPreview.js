1c63b2534a38f6073578b8f23b5f6d175ad17c34
unknown <pauline.desert@hotmail.fr>
2020-06-12 12:32:24 +0200

    Anim galerie

--- assets/uploadPreview.js
+++ assets/uploadPreview.js
@@ -16,14 +16,3 @@ $(function() {
 		};
 	});
 })
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.materialboxed');
-    var instances = M.Materialbox.init(elems, options);
-  });
-
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('.materialboxed').materialbox();
-  });
--- galery.php
+++ galery.php
@@ -4,7 +4,7 @@ function updateGalery() {
     $imgGalery = array_diff(scandir('img'), array('..', '.'));
 
     foreach($imgGalery as $img) {
-        echo '<img class="col s4 responsive-img z-depth-2 materialboxed" src="img/'. $img .'">';
+        echo '<img class="col s4 responsive-img z-depth-2 materialboxed" width="650"  src="img/'. $img .'">';
     }
 }
  
@@ -33,9 +33,15 @@ function updateGalery() {
     </div>
 
 
+    
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="assets/uploadPreview.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var elems = document.querySelectorAll('.materialboxed');
+            var instances = M.Materialbox.init(elems);
+        });
+    </script>
 </body>
 
 </html>
